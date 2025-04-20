f60d55047b51ec02711506b9e9eb5214cb097430
Azamat-JS <azamatielts987@gmail.com>
2025-04-19 18:53:07 -0700

    added

---
+++ PrismaOrm/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3000;
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  }));
+  await app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/api`);
+  }
+  );
+}
+bootstrap();
