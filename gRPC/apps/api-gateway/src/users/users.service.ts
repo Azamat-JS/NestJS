683b0097fe98e809a199944aa8f1a221407b0fe6
Azamat-JS <azamatielts987@gmail.com>
2025-05-16 16:16:54 +0500

    gRPC completed

--- gRPC/apps/api-gateway/src/users/users.service.ts
+++ gRPC/apps/api-gateway/src/users/users.service.ts
@@ -1,25 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto, UpdateUserDto } from '@app/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { CreateUserDto, PaginationDto, UpdateUserDto, USERS_SERVICE_NAME, UsersServiceClient } from '@app/common';
+import { AUTH_SERVICE } from './constants';
+import { ClientGrpcProxy } from '@nestjs/microservices';
+import { ReplaySubject } from 'rxjs';
 
 @Injectable()
-export class UsersService {
+export class UsersService implements OnModuleInit{
+  private usersService: UsersServiceClient;
+
+  constructor(@Inject(AUTH_SERVICE) private client: ClientGrpcProxy){}
+
+  onModuleInit() {
+    this.usersService = this.client.getService<UsersServiceClient>(USERS_SERVICE_NAME)
+  }
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.usersService.createUser(createUserDto);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.usersService.findAllUsers({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.usersService.findOneUser({id})
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUser({...updateUserDto, id});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.usersService.removeUser({id});
+  }
+
+  emailUsers(){
+    const users$ = new ReplaySubject<PaginationDto>();
+
+    users$.next({page: 0, skip: 25});
+    users$.next({page: 1, skip: 25});
+    users$.next({page: 2, skip: 25});
+    users$.next({page: 3, skip: 25});
+
+    users$.complete();
+
+    let chunkNumber = 1;
+
+    this.usersService.queryUsers(users$).subscribe(users => {
+      console.log('Chunk', chunkNumber, users);
+      chunkNumber += 1;
+    })
   }
 }
--- gRPC/dist/apps/api-gateway/src/main.js
+++ gRPC/dist/apps/api-gateway/src/main.js
@@ -71,7 +71,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.UsersController = void 0;
 const common_1 = __webpack_require__(/*! @nestjs/common */ "@nestjs/common");
 const users_service_1 = __webpack_require__(/*! ./users.service */ "./apps/api-gateway/src/users/users.service.ts");
-const common_2 = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module '@app/common'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+const common_2 = __webpack_require__(/*! @app/common */ "./libs/common/src/index.ts");
 let UsersController = class UsersController {
     usersService;
     constructor(usersService) {
@@ -84,13 +84,16 @@ let UsersController = class UsersController {
         return this.usersService.findAll();
     }
     findOne(id) {
-        return this.usersService.findOne(+id);
+        return this.usersService.findOne(id);
     }
     update(id, updateUserDto) {
-        return this.usersService.update(+id, updateUserDto);
+        return this.usersService.update(id, updateUserDto);
     }
     remove(id) {
-        return this.usersService.remove(+id);
+        return this.usersService.remove(id);
+    }
+    emailUsers() {
+        return this.usersService.emailUsers();
     }
 };
 exports.UsersController = UsersController;
@@ -129,6 +132,12 @@ __decorate([
     __metadata("design:paramtypes", [String]),
     __metadata("design:returntype", void 0)
 ], UsersController.prototype, "remove", null);
+__decorate([
+    (0, common_1.Post)('email'),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], UsersController.prototype, "emailUsers", null);
 exports.UsersController = UsersController = __decorate([
     (0, common_1.Controller)('users'),
     __metadata("design:paramtypes", [typeof (_a = typeof users_service_1.UsersService !== "undefined" && users_service_1.UsersService) === "function" ? _a : Object])
@@ -157,7 +166,7 @@ const users_service_1 = __webpack_require__(/*! ./users.service */ "./apps/api-g
 const users_controller_1 = __webpack_require__(/*! ./users.controller */ "./apps/api-gateway/src/users/users.controller.ts");
 const microservices_1 = __webpack_require__(/*! @nestjs/microservices */ "@nestjs/microservices");
 const constants_1 = __webpack_require__(/*! ./constants */ "./apps/api-gateway/src/users/constants.ts");
-const common_2 = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module '@app/common'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+const common_2 = __webpack_require__(/*! @app/common */ "./libs/common/src/index.ts");
 const path_1 = __webpack_require__(/*! path */ "path");
 let UsersModule = class UsersModule {
 };
@@ -197,30 +206,150 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+var _a;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.UsersService = void 0;
 const common_1 = __webpack_require__(/*! @nestjs/common */ "@nestjs/common");
+const common_2 = __webpack_require__(/*! @app/common */ "./libs/common/src/index.ts");
+const constants_1 = __webpack_require__(/*! ./constants */ "./apps/api-gateway/src/users/constants.ts");
+const microservices_1 = __webpack_require__(/*! @nestjs/microservices */ "@nestjs/microservices");
+const rxjs_1 = __webpack_require__(/*! rxjs */ "rxjs");
 let UsersService = class UsersService {
+    client;
+    usersService;
+    constructor(client) {
+        this.client = client;
+    }
+    onModuleInit() {
+        this.usersService = this.client.getService(common_2.USERS_SERVICE_NAME);
+    }
     create(createUserDto) {
-        return 'This action adds a new user';
+        return this.usersService.createUser(createUserDto);
     }
     findAll() {
-        return `This action returns all users`;
+        return this.usersService.findAllUsers({});
     }
     findOne(id) {
-        return `This action returns a #${id} user`;
+        return this.usersService.findOneUser({ id });
     }
     update(id, updateUserDto) {
-        return `This action updates a #${id} user`;
+        return this.usersService.updateUser({ ...updateUserDto, id });
     }
     remove(id) {
-        return `This action removes a #${id} user`;
+        return this.usersService.removeUser({ id });
+    }
+    emailUsers() {
+        const users$ = new rxjs_1.ReplaySubject();
+        users$.next({ page: 0, skip: 25 });
+        users$.next({ page: 1, skip: 25 });
+        users$.next({ page: 2, skip: 25 });
+        users$.next({ page: 3, skip: 25 });
+        users$.complete();
+        let chunkNumber = 1;
+        this.usersService.queryUsers(users$).subscribe(users => {
+            console.log('Chunk', chunkNumber, users);
+            chunkNumber += 1;
+        });
     }
 };
 exports.UsersService = UsersService;
 exports.UsersService = UsersService = __decorate([
-    (0, common_1.Injectable)()
+    (0, common_1.Injectable)(),
+    __param(0, (0, common_1.Inject)(constants_1.AUTH_SERVICE)),
+    __metadata("design:paramtypes", [typeof (_a = typeof microservices_1.ClientGrpcProxy !== "undefined" && microservices_1.ClientGrpcProxy) === "function" ? _a : Object])
 ], UsersService);
+
+
+/***/ }),
+
+/***/ "./libs/common/src/index.ts":
+/*!**********************************!*\
+  !*** ./libs/common/src/index.ts ***!
+  \**********************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(/*! ./types */ "./libs/common/src/types/index.ts"), exports);
+
+
+/***/ }),
+
+/***/ "./libs/common/src/types/auth.ts":
+/*!***************************************!*\
+  !*** ./libs/common/src/types/auth.ts ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.USERS_SERVICE_NAME = exports.AUTH_PACKAGE_NAME = exports.protobufPackage = void 0;
+exports.UsersServiceControllerMethods = UsersServiceControllerMethods;
+const microservices_1 = __webpack_require__(/*! @nestjs/microservices */ "@nestjs/microservices");
+exports.protobufPackage = "auth";
+exports.AUTH_PACKAGE_NAME = "auth";
+function UsersServiceControllerMethods() {
+    return function (constructor) {
+        const grpcMethods = ["createUser", "findAllUsers", "findOneUser", "updateUser", "removeUser"];
+        for (const method of grpcMethods) {
+            const descriptor = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+            (0, microservices_1.GrpcMethod)("UsersService", method)(constructor.prototype[method], method, descriptor);
+        }
+        const grpcStreamMethods = ["queryUsers"];
+        for (const method of grpcStreamMethods) {
+            const descriptor = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+            (0, microservices_1.GrpcStreamMethod)("UsersService", method)(constructor.prototype[method], method, descriptor);
+        }
+    };
+}
+exports.USERS_SERVICE_NAME = "UsersService";
+
+
+/***/ }),
+
+/***/ "./libs/common/src/types/index.ts":
+/*!****************************************!*\
+  !*** ./libs/common/src/types/index.ts ***!
+  \****************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(/*! ./auth */ "./libs/common/src/types/auth.ts"), exports);
 
 
 /***/ }),
@@ -262,6 +391,16 @@ module.exports = require("@nestjs/microservices");
 /***/ ((module) => {
 
 module.exports = require("path");
+
+/***/ }),
+
+/***/ "rxjs":
+/*!***********************!*\
+  !*** external "rxjs" ***!
+  \***********************/
+/***/ ((module) => {
+
+module.exports = require("rxjs");
 
 /***/ })
 
