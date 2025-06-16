777d3a04314e47b513a220911eafe88e7fdc9fe5
Azamat-JS <azamatielts987@gmail.com>
2025-06-16 11:04:00 +0500

    added function to handle tasks

--- Bot_nest/src/app.utils.ts
+++ Bot_nest/src/app.utils.ts
@@ -1,3 +1,6 @@
 export const showList = todos => 
     `Your todo list: \n\n${todos
     .map(todo => (todo.isCompleted ? '✅' : '📍') + ' ' + todo.name + `\n\n`).join('')}`
+
+
+export const showTask = task => (task.isCompleted ? '✅' : '📍') + ' ' + task.name
